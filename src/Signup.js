e5065b6082228be5333cc94d3d697ba11bf7dd07
Ayush Gautam <gautamayush953@gmail.com>
2022-04-14 01:45:09 +0530

    updated

--- src/Signup.js
+++ src/Signup.js
@@ -1,92 +1,193 @@
 import React, { useState } from 'react';
-import './signup.css';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import { Link } from 'react-router-dom';
+
+import {Link} from 'react-router-dom';
+
 import EmailIcon from '@mui/icons-material/Email';
 import SchoolIcon from '@mui/icons-material/School';
+import PhoneIcon from '@mui/icons-material/Phone';
+import PasswordIcon from '@mui/icons-material/Password';
+import { IconButton } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
-import { IconButton } from '@mui/material';
+
+import './signup.css';
+
+import { TextField } from '@mui/material';
 
 const SignUp = () => {
 
   const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState("")
   const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [shown, setShown] = useState(false)
-  const [shownName, setShownName] = useState(false)
-  const [previous, setPrevious] = useState(false)
-  const [passwordVisible, setPasswordVisible] = useState(false)  
-  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false)  
+  const [email, setEmail] = useState('') 
 
-  const changeVisibilityPassword = ()=> {
-    if (passwordVisible === false) {
-        setPasswordVisible(true)
-    }
-    else {
-        setPasswordVisible(false)
-    }
+  const [passwordVisible, setPasswordVisible] = useState({
+    password: "",
+    showPassword: false
+  })
+
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState({
+    confirmPassword: "",
+    showConfirmPassword: false
+  })
+
+
+  const changeVisibility = ()=> {
+      setPasswordVisible({...passwordVisible, showPassword: !passwordVisible.showPassword});
+  }
+  
+
+  const contentVisibility = (event) => {
+      setPasswordVisible({...passwordVisible, password: event.target.value});
   }
 
-  const changeVisibilityConfirmPassword = ()=> {
-    if (confirmPasswordVisible === false) {
-        setConfirmPasswordVisible(true)
-    }
-    else {
-        setConfirmPasswordVisible(false)
-
-    }
+  const changeVisibilityConfirm = () => { 
+    setConfirmPasswordVisible({...confirmPasswordVisible, showConfirmPassword: !confirmPasswordVisible.showConfirmPassword});
   }
 
-  const nonVisibleConfirm = <FontAwesomeIcon className='visibility-password' value={confirmPasswordVisible} onClick={changeVisibilityConfirmPassword} icon={faEyeSlash} style={{color: 'aquamarine'}}/>
-  const visibleConfirm = <FontAwesomeIcon className='visibility-password' value={confirmPasswordVisible} onClick={changeVisibilityConfirmPassword} icon={faEye} style={{color: 'aquamarine'}}/>
-
-  const nonVisible = <FontAwesomeIcon className='visibility-password' value={passwordVisible} onClick={changeVisibilityPassword} icon={faEyeSlash} style={{color: 'aquamarine'}}/>
-  const visible = <FontAwesomeIcon className='visibility-password' value={passwordVisible} onClick={changeVisibilityPassword} icon={faEye} style={{color: 'aquamarine'}}/>
+  const contentVisibilityConfirm = (event) => {
+    setConfirmPasswordVisible({...confirmPasswordVisible, confirmPassword: event.target.value});
+  }
 
   return (
-      <div id="signup-body">
+    <div id="signup-body">
+      <div className="signup-main-items" id="signup-heading">Sign Up Page</div>
 
-        <Link to='/'><ArrowLeftIcon onMouseEnter={() => setPrevious(true)} onMouseLeave={() => setPrevious(false)} id='previous-icon' /></Link>
-        {previous && (<div className='hover-icon'>Previous Page</div>)}
-        <div id="signup-heading">Sign Up Page</div>
-        <div>
-          
-          <div style={{border: '1px solid red'}}>
-            <SchoolIcon style={{border: '1px solid red'}} onMouseEnter={() => setShownName(true)} onMouseLeave={() => setShownName(false)} className='main-icons'/>
-            <input type="text" name='institute-name' className='signup-main' autoComplete='off' value={name} onChange={(e) => { setName(e.target.value) } } placeholder='Institute Name...'/><br /><br />
-          </div>
-          {shownName && (<div className='hover-icon'>Name</div>)}
+      <div className="signup-main-items" id="signup-form">
+        <div className="signup-form-div1" id="signup-form-name-div">
+          <SchoolIcon className="signup-form-icon"/>
+          <TextField
+          className="signup-form-textfield"
+          label="Institute Name"
+          variant="standard"
+          spellcheck={false}
+          sx = {{
+            input: {
+              color: 'aquamarine',
+              fontFamily: 'Libre Baskerville, serif',
+              fontSize: 18,
+              marginTop: '4px'
+            },
+            label: {
+              color: "#aaaaaa",
+              fontSize: 20,
+              fontFamily: 'Libre Baskerville, serif',
+            }
+          }}
+          />  
+        </div>
 
+        <div className="signup-form-div1">
+          <EmailIcon className="signup-form-icon"/>
+          <TextField 
+          className="signup-form-textfield"
+          label="Institute Email Id"
+          variant="standard"
+          spellcheck={false}
+          sx = {{
+            input: {
+              color: 'aquamarine',
+              fontFamily: 'Libre Baskerville, serif',
+              fontSize: 18,
+              marginTop: '4px'
+            },
+            label: {
+              color: "#aaaaaa",
+              fontSize: 20,
+              fontFamily: 'Libre Baskerville, serif',
+            }
+          }}
+          />
+        </div>
 
-          <div className="detail-fields">
-            <EmailIcon onMouseEnter={() => setShown(true)} onMouseLeave={() => setShown(false)} className='main-icons'/>
-            <input type="text" name='institute-name' className='signup-main' autoComplete='off' value={email} onChange={(e) => { setEmail(e.target.value) } } placeholder='Institute Email Id...'/><br /><br />  
-          </div>
-          {shown && (<div className='hover-icon'>Email</div>)}
+        <div className="signup-form-div1">
+          <PhoneIcon className="signup-form-icon"/>
+          <TextField 
+          className="signup-form-textfield"
+          label="Institute Contact Number"
+          variant="standard"
+          spellcheck={false}
+          sx = {{
+            input: {
+              color: 'aquamarine',
+              fontFamily: 'Libre Baskerville, serif',
+              fontSize: 18,
+              marginTop: '4px'
+            },
+            label: {
+              color: "#aaaaaa",
+              fontSize: 20,
+              fontFamily: 'Libre Baskerville, serif',
+            }
+          }}
+          />
+        </div>
 
+        <div className="signup-form-div1">
+          <PasswordIcon className="signup-form-icon"/>
+          <TextField 
+          value={passwordVisible.password}
+          type={passwordVisible.showPassword ? 'text' : 'password'}
+          className="signup-form-textfield"
+          label="Password"
+          variant="standard"
+          onChange={contentVisibility}
+          spellcheck={false}
+          InputProps={{
+            endAdornment: passwordVisible.showPassword ? <IconButton><Visibility style={{color: 'aquamarine'}} onClick={changeVisibility}/></IconButton> : <IconButton><VisibilityOff style={{color: 'aquamarine'}} onClick={changeVisibility}/></IconButton>
+          }}
+          sx = {{
+            input: {
+              color: 'aquamarine',
+              fontFamily: 'Libre Baskerville, serif',
+              fontSize: 18,
+              marginTop: '4px'
+            },
+            label: {
+              color: "#aaaaaa",
+              fontSize: 20,
+              fontFamily: 'Libre Baskerville, serif',
+            }
+          }}
+          />
+        </div>
 
-          {/* Password And Confirm Password Input Fields */}
-          <div id='password-fields'>
-            <div className='div-password'>
-            <input type={passwordVisible ? "text" : "password"} className='signup-password'  autoComplete='off' onChange={(e) => { setPassword(e.target.value) } } value = {password} placeholder='Password...'/> 
-            <IconButton className='div-password-fields' style={{fontSize: 16, marginLeft: '-10%'}}>{passwordVisible ? visible : nonVisible}</IconButton>
-            </div>
+        <div className="signup-form-div1">
+          <PasswordIcon className="signup-form-icon"/>
+          <TextField 
+          value={confirmPasswordVisible.confirmPassword}
+          type={confirmPasswordVisible.showConfirmPassword ? 'text' : 'password'}
+          className="signup-form-textfield"
+          label="Confirm Password"
+          variant="standard"
+          onChange={contentVisibilityConfirm}
+          spellcheck={false}
+          InputProps={{
+            endAdornment: confirmPasswordVisible.showConfirmPassword ? <IconButton><Visibility style={{color: 'aquamarine'}} onClick={changeVisibilityConfirm}/></IconButton> : <IconButton><VisibilityOff style={{color: 'aquamarine'}} onClick={changeVisibilityConfirm}/></IconButton>
+          }}
+          sx = {{
+            input: {
+              color: 'aquamarine',
+              fontFamily: 'Libre Baskerville, serif',
+              fontSize: 18,
+              marginTop: '4px'
+            },
+            label: {
+              color: "#aaaaaa",
+              fontSize: 20,
+              fontFamily: 'Libre Baskerville, serif',
+            }
+          }}
+          />
+        </div>
 
-            <div className='div-password' id= "signin-confirm-password">
-            <input type={confirmPasswordVisible ? "text" : "password"} id= "signin-confirm-password" className='signup-password' autoComplete='off' onChange={(e) => { setConfirmPassword(e.target.value) }} value={confirmPassword} placeholder='Confirm Password...'/>
-            <IconButton className='div-password-fields' style={{fontSize: 16, marginLeft: '-10%'}}>{confirmPasswordVisible ? visibleConfirm : nonVisibleConfirm}</IconButton>
-            </div>
-          </div><br /><br />
-          
-
-          {/* Submit Button */}
-          <div id='submit-button-div'>
-            <Link to="/login/profile"><button id='submit-button'>Sign Up</button></Link>
-          </div>
-
+        <div id="signup-form-first-page">
+          <Link to="/SecondSignUpPage"><button id="button">Next</button></Link>
+        </div>
       </div>
     </div>
   );
